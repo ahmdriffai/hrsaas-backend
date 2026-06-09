0914831a59257408bcab9956c16daba861894798
yokunoshita <hanafiulinnuha1812@gmail.com>
2026-06-09 13:16:44 +0700

    feat: add employment status

--- internal/entity/employee.go
+++ internal/entity/employee.go
@@ -49,18 +49,19 @@ func (c *Employee) TableName() string {
 }
 
 type EmployeeContract struct {
-	ID           string   `gorm:"column:id;primaryKey"`
-	EmployeeID   string   `gorm:"column:employee_id;not null"`
-	ContractType string   `gorm:"column:contract_type;not null"`
-	StartDate    int64    `gorm:"column:start_date;not null"`
-	EndDate      *int64   `gorm:"column:end_date"`
-	DivisionID   string   `gorm:"column:division_id;not null"`
-	PositionID   string   `gorm:"column:position_id;not null"`
-	Salary       float64  `gorm:"column:salary;not null"`
-	Employee     Employee `gorm:"foreignKey:EmployeeID;references:ID"`
-	Division     Division `gorm:"foreignKey:DivisionID;references:ID"`
-	Position     Position `gorm:"foreignKey:PositionID;references:ID"`
-	IsActive     bool     `gorm:"column:is_active;not null"`
+	ID               string   `gorm:"column:id;primaryKey"`
+	EmployeeID       string   `gorm:"column:employee_id;not null"`
+	ContractType     string   `gorm:"column:contract_type;not null"`
+	StartDate        int64    `gorm:"column:start_date;not null"`
+	EndDate          *int64   `gorm:"column:end_date"`
+	DivisionID       string   `gorm:"column:division_id;not null"`
+	PositionID       string   `gorm:"column:position_id;not null"`
+	EmploymentStatus string   `gorm:"column:employee_status;not null"`
+	Salary           float64  `gorm:"column:salary;not null"`
+	Employee         Employee `gorm:"foreignKey:EmployeeID;references:ID"`
+	Division         Division `gorm:"foreignKey:DivisionID;references:ID"`
+	Position         Position `gorm:"foreignKey:PositionID;references:ID"`
+	IsActive         bool     `gorm:"column:is_active;not null"`
 }
 
 // BeforeCreate hook to set UUID
--- internal/model/employee_contract_model.go
+++ internal/model/employee_contract_model.go
@@ -3,39 +3,42 @@ package model
 import "hr-sas/internal/entity"
 
 type EmployeeContractResponse struct {
-	ID           string           `json:"id"`
-	EmployeeID   string           `json:"employee_id"`
-	ContractType string           `json:"contract_type"`
-	StartDate    int64            `json:"start_date"`
-	EndDate      *int64           `json:"end_date,omitempty"`
-	DivisionID   string           `json:"division_id"`
-	PositionID   string           `json:"position_id"`
-	Salary       float64          `json:"salary"`
-	IsActive     bool             `json:"is_active"`
-	Employee     EmployeeResponse `json:"employee,omitempty"`
-	User         *UserResponse    `json:"user,omitempty"`
-	Division     DivisionResponse `json:"division,omitempty"`
-	Position     PositionResponse `json:"position,omitempty"`
+	ID               string           `json:"id"`
+	EmployeeID       string           `json:"employee_id"`
+	ContractType     string           `json:"contract_type"`
+	StartDate        int64            `json:"start_date"`
+	EndDate          *int64           `json:"end_date,omitempty"`
+	DivisionID       string           `json:"division_id"`
+	PositionID       string           `json:"position_id"`
+	EmploymentStatus string           `json:"employee_status"`
+	Salary           float64          `json:"salary"`
+	IsActive         bool             `json:"is_active"`
+	Employee         EmployeeResponse `json:"employee,omitempty"`
+	User             *UserResponse    `json:"user,omitempty"`
+	Division         DivisionResponse `json:"division,omitempty"`
+	Position         PositionResponse `json:"position,omitempty"`
 }
 
 type CreateEmployeeContractRequest struct {
-	EmployeeID   string  `json:"employee_id" validate:"required"`
-	ContractType string  `json:"contract_type" validate:"required"`
-	StartDate    string  `json:"start_date" validate:"required"`
-	EndDate      *string `json:"end_date"`
-	DivisionID   string  `json:"division_id" validate:"required"`
-	PositionID   string  `json:"position_id" validate:"required"`
-	Salary       float64 `json:"salary" validate:"required,min=0"`
+	EmployeeID       string  `json:"employee_id" validate:"required"`
+	ContractType     string  `json:"contract_type" validate:"required"`
+	StartDate        string  `json:"start_date" validate:"required"`
+	EndDate          *string `json:"end_date"`
+	DivisionID       string  `json:"division_id" validate:"required"`
+	PositionID       string  `json:"position_id" validate:"required"`
+	EmploymentStatus string  `json:"employee_status" validate:"required"`
+	Salary           float64 `json:"salary" validate:"required,min=0"`
 }
 
 type UpdateEmployeeContractRequest struct {
-	ContractType *string  `json:"contract_type,omitempty"`
-	StartDate    *string  `json:"start_date,omitempty"`
-	EndDate      *string  `json:"end_date,omitempty"`
-	DivisionID   *string  `json:"division_id,omitempty"`
-	PositionID   *string  `json:"position_id,omitempty"`
-	Salary       *float64 `json:"salary,omitempty" validate:"omitempty,min=0"`
-	IsActive     *bool    `json:"is_active,omitempty"`
+	ContractType     *string  `json:"contract_type,omitempty"`
+	StartDate        *string  `json:"start_date,omitempty"`
+	EndDate          *string  `json:"end_date,omitempty"`
+	DivisionID       *string  `json:"division_id,omitempty"`
+	PositionID       *string  `json:"position_id,omitempty"`
+	Salary           *float64 `json:"salary,omitempty" validate:"omitempty,min=0"`
+	EmploymentStatus *string  `json:"employee_status,omitempty"`
+	IsActive         *bool    `json:"is_active,omitempty"`
 }
 
 type SearchEmployeeContractRequest struct {
@@ -55,15 +58,16 @@ func EmployeeContractToResponse(contract *entity.EmployeeContract) *EmployeeCont
 	user := UserToResponse(&contract.Employee.User)
 
 	response := &EmployeeContractResponse{
-		ID:           contract.ID,
-		EmployeeID:   contract.EmployeeID,
-		ContractType: contract.ContractType,
-		StartDate:    contract.StartDate,
-		EndDate:      contract.EndDate,
-		DivisionID:   contract.DivisionID,
-		PositionID:   contract.PositionID,
-		Salary:       contract.Salary,
-		IsActive:     contract.IsActive,
+		ID:               contract.ID,
+		EmployeeID:       contract.EmployeeID,
+		ContractType:     contract.ContractType,
+		StartDate:        contract.StartDate,
+		EndDate:          contract.EndDate,
+		DivisionID:       contract.DivisionID,
+		PositionID:       contract.PositionID,
+		EmploymentStatus: contract.EmploymentStatus,
+		Salary:           contract.Salary,
+		IsActive:         contract.IsActive,
 	}
 
 	response.User = user
